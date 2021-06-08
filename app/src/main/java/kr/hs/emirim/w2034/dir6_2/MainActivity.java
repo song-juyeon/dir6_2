2be60414d9de53bfc5fa98b5f8f7ec24708986f7
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-06-08 19:46:35 +0900

    completion dir6_2 commit

---
+++ app/src/main/java/kr/hs/emirim/w2034/dir6_2/MainActivity.java
@@ -0,0 +1,39 @@
+package kr.hs.emirim.w2034.dir6_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        flipper = findViewById(R.id.flipper);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnStop = findViewById(R.id.btn_stop);
+
+        btnStart.setOnClickListener(btnListener);
+        btnStop.setOnClickListener(btnListener);
+    }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_start:
+                    flipper.startFlipping();
+                    break;
+                case R.id.btn_stop:
+                    flipper.stopFlipping();
+                    break;
+            }
+        }
+    };
+}
